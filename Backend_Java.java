591645bcfbc3c40c301a98e302a3acdfb59edaeb
Sahiti <69972167+Sahitidwivedula@users.noreply.github.com>
2024-01-11 14:58:04 -0600

    Backend_Java.java

--- Backend_Java.java
+++ Backend_Java.java
@@ -2,16 +2,76 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
+// package com.mycompany.backend_java;
+// import java.util.*;
+// public class Backend_Java{
+//     public static void main(String args[]){
+//     // output
+//     System.out.print("Hello world with java!\n"); //using /n
+//     System.out.println("Hello world with java!"); //using println
 
-package com.mycompany.backend_java;
+//     //print the pattern: Just to print the *
+//     System.out.println("*");
+//     System.out.println("**");
+//     System.out.println("***");
+//     System.out.println("****");
 
-/**
- *
- * @author sahit
- */
-public class Backend_Java {
+//     // //variables: int, double, string:datatypes
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+//     // //Input: Scanner
+//     Scanner sc = new Scanner(System.in); 
+//     //String name1 = sc.next(); //this will only scan for 1 word and stop so we use,
+//     String name2 = sc.nextLine();
+//     System.out.println(name2);
+//     // //nextint(), nextfloat();
+//     sc.close();
+
+// //Task-1
+// Scanner scan = new Scanner(System.in);
+//     System.out.println("Enter the value of k:");
+//     int k = scan.nextInt();
+//     System.out.println("Enter the value of z:");
+//     int z= scan.nextInt();    
+//     int add=k+z;
+//     System.out.println("the sum is:");
+//     System.out.println(add);
+//     scan.close();
+    
+// // Make a program that takes the radius of a circle as input, calculates its radius and area and prints it as output to the user.
+// Scanner scan1 = new Scanner(System.in);
+// System.out.println("Enter the radius value:");
+// int radius=scan1.nextInt();
+// Double Area= 3.14*radius*radius;
+// System.out.println("Area of the circle is:");
+// System.out.println(Area);
+// scan1.close();
+
+// // Make a program that prints the table of a number that is input by the user. (without conditions or loops)
+// Scanner scanner= new Scanner(System.in);
+// System.out.println("Enter the number:");
+// int o=scanner.nextInt();
+// int c = o*2;
+// System.out.println(c);
+// int d = o*3;
+// System.out.println(d);
+// int e = o*4;
+// System.out.println(e);
+// int f = o*5;
+// System.out.println(f);
+// int g = o*6;
+// System.out.println(g);
+// int h = o*7;
+// System.out.println(h);
+// int i = o*8;
+// System.out.println(i);
+// int j = o*9;
+// System.out.println(j);
+// int l = o*10;
+// System.out.println(l);
+// scanner.close();
+// //The above code works, but it takes up a lot of lines to write it, in the next topic, I learn how to do this more effectively using conditional and loop statements
+
+// //Solved more examples from HackerRank
+//   }      
+// }
+
